11255440ae09c0e25644fd01811a790aaec09cce
Lucas Alves <lucasalves_taz@hotmail.com>
2021-06-25 14:08:06 -0300

    add

--- __main__.py
+++ __main__.py
@@ -1,14 +1,15 @@
 import __conectdb__
-import __log__
+#import __log__
 import __list__
 import backoff
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+import time
 
-@backoff.on_exception(backoff.expo,(Exception,TimeoutError), max_tries=10)
+#@backoff.on_exception(backoff.expo,(Exception,TimeoutError), max_tries=10)
 def dados():
     options = FirefoxOptions()
-    options.add_argument("--headless")
+    #options.add_argument("--headless")
     web = webdriver.Firefox(options=options)
 
     web.implicitly_wait(20)
@@ -16,7 +17,27 @@ def dados():
     url = 'https://fundamentus.com.br/'
     web.get(url)
 
-    acao = __list__.list
+    acao = __list__.lst_acao
 
     for i in acao:
-        web.fin
+
+        time.sleep(2)
+        web.find_element_by_xpath('/html/body/div[1]/div[1]/form/fieldset/input[1]').send_keys(i)
+        web.find_element_by_xpath('/html/body/div[1]/div[1]/form/fieldset/input[2]').click()
+
+        papel = web.find_element_by_xpath('/html/body/div[1]/div[2]/table[1]/tbody/tr[1]/td[2]/span')
+        papel_text = papel.text
+        print(papel_text)
+
+        query_insert_bd = f" INSERT INTO dados VALUES ('{papel_text}') "
+
+        #query_d = " INSERT INTO acao VALUES ( '"+papel_text+"',
+        __conectdb__.in_dados(query_insert_bd)
+
+dados()
+
+
+
+
+
+        
