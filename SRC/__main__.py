f381bdce4fed7ed61d1c2bff1d288d5f9cb3e1b0
Lucas Alves <lucasalves_taz@hotmail.com>
2021-06-29 13:58:52 -0300

    add

--- SRC/__main__.py
+++ SRC/__main__.py
@@ -48,9 +48,15 @@ def dados():
 
             papel = web.find_element_by_xpath('/html/body/div[1]/div[2]/table[1]/tbody/tr[1]/td[2]/span')
             papel_text = papel.text
+            tipo = web.find_element_by_xpath('/html/body/div[1]/div[2]/table[1]/tbody/tr[2]/td[2]/span')
+            tipo_text = tipo.text
+            empresa = web.find_element_by_xpath('/html/body/div[1]/div[2]/table[1]/tbody/tr[3]/td[2]/span')
+            empresa_text = empresa.text
+            setor = web.find_element_by_xpath('/html/body/div[1]/div[2]/table[1]/tbody/tr[4]/td[2]/span/a')
+            setor_text = setor.text
 
             #Insere os dados coletados no banco de dados#
-            query_insert_bd = f" INSERT INTO dados VALUES ('{dt}','{papel_text}') "
+            query_insert_bd = f" INSERT INTO dados VALUES ('{dt}','{papel_text}','{tipo_text}','{empresa_text}','{setor_text}') "
             __conectdb__.in_dados(query_insert_bd)
             print(f"+{GREEN} dados da ação: {i}, gravados com sucesso {RESET}+")
             # --- #
