94a08c54f89d7daef844789cb7ad6635b6e8da0e
Lucas Alves <lucasalves_taz@hotmail.com>
2021-06-29 21:47:57 -0300

    add

--- Doc/colunasbd.md
+++ Doc/colunasbd.md
@@ -4,12 +4,39 @@
 
 ## Colunas
 
-- data_dado_inserido:
-1. Descrição -> data em que o dado foi inserido no banco de dados :white_check_mark:
+- (1) data_dado_inserido:
+1. Descrição -> data em que o dado foi inserido no banco de dados. :white_check_mark:
 2. Tipo(type) -> date
 
-- papel:
-1. Descrição -> ticker da ação negociada na B3
+- (2) papel:
+1. Descrição -> ticker da ação negociada na B3.
 2. Tipo(type) -> varchar(255)
 
-- 
+- (3) tipo:
+1. Descrição -> tipo da ação(ON / PN / UNIT).
+https://comoinvestir.thecap.com.br/diferenca-entre-acoes-preferenciais-ordinarias-units/
+2. Tipo(Type) -> varchar(255)
+
+- (4) empresa:
+1. Descrição -> nome da empresa.
+2. Tipo(Type) -> varchar(255)
+
+- (5) setor:
+1. Descrição -> setor em que a empresa atua
+2. Tipo(type) -> varchar(255)
+
+- (6) cotacao:
+1. Descrição -> valor da ação no dia em que foi coletado os dado.
+2. Tipo(type) -> money
+
+- (7) data_ult_cotacao
+1. Descrição -> data da última negociação da ação
+2. Tipo(Type) -> date
+
+- (8) min_52_sem:
+1. Descrição -> menor valor da cotação em 52 semanas
+2. Tipo(type) -> money
+
+- (9) max_52_sem:
+1. Descrição -> maior valor da cotação em 52 semanas
+2. Tipo(type) -> money
--- SRC/__main__.py
+++ SRC/__main__.py
@@ -5,7 +5,8 @@ import backoff
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 import time
-from datetime import date
+import datetime
+from datetime import date, datetime
 
 RED   = "\033[1;31m"  
 GREEN = "\033[0;32m"
@@ -15,7 +16,7 @@ YELLOW = "\033[1;33m"
 #@backoff.on_exception(backoff.expo,(Exception,TimeoutError), max_tries=10)
 def dados():
     options = FirefoxOptions()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     web = webdriver.Firefox(options=options)
 
     web.implicitly_wait(20)
@@ -42,29 +43,44 @@ def dados():
 
         else:    
 
+            print(dt)
+
             time.sleep(2)
             web.find_element_by_xpath('/html/body/div[1]/div[1]/form/fieldset/input[1]').send_keys(i)
             web.find_element_by_xpath('/html/body/div[1]/div[1]/form/fieldset/input[2]').click()
-
+            
+            # Inicio da coleta dos dados #
             papel = web.find_element_by_xpath('/html/body/div[1]/div[2]/table[1]/tbody/tr[1]/td[2]/span')
             papel_text = papel.text
+            #
             tipo = web.find_element_by_xpath('/html/body/div[1]/div[2]/table[1]/tbody/tr[2]/td[2]/span')
             tipo_text = tipo.text
+            #
             empresa = web.find_element_by_xpath('/html/body/div[1]/div[2]/table[1]/tbody/tr[3]/td[2]/span')
             empresa_text = empresa.text
+            #
             setor = web.find_element_by_xpath('/html/body/div[1]/div[2]/table[1]/tbody/tr[4]/td[2]/span/a')
             setor_text = setor.text
+            #
+            cotação = web.find_element_by_xpath('/html/body/div[1]/div[2]/table[1]/tbody/tr[1]/td[4]/span')
+            cotação_text = cotação.text.replace(',','.')
+            #
+            dt_ult_cotacao = web.find_element_by_xpath('/html/body/div[1]/div[2]/table[1]/tbody/tr[2]/td[4]/span')
+            dt_ult_cotacao_text = dt_ult_cotacao.text
+            dt_ult_cotacao_tr = datetime.strptime(dt_ult_cotacao_text, '%d/%m/%Y').date()
+            #
+            min_52_sem = web.find_element_by_xpath('/html/body/div[1]/div[2]/table[1]/tbody/tr[3]/td[4]/span')
+            min_52_sem_text = min_52_sem.text.replace(',','.')
+            #
+            max_52_sem = web.find_element_by_xpath('/html/body/div[1]/div[2]/table[1]/tbody/tr[4]/td[4]/span')
+            max_52_sem_text = max_52_sem.text.replace(',','.')
+            #
 
             #Insere os dados coletados no banco de dados#
-            query_insert_bd = f" INSERT INTO dados VALUES ('{dt}','{papel_text}','{tipo_text}','{empresa_text}','{setor_text}') "
+            query_insert_bd = f" INSERT INTO dados VALUES ('{dt}','{papel_text}','{tipo_text}','{empresa_text}',\
+            '{setor_text}','{cotação_text}','{dt_ult_cotacao_tr}','{min_52_sem_text}','{max_52_sem_text}') "
             __conectdb__.in_dados(query_insert_bd)
             print(f"+{GREEN} dados da ação: {i}, gravados com sucesso {RESET}+")
             # --- #
 
 dados()
-
-
-
-
-
-        
