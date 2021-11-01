4a8070edb13effc30b5811f7f451742090755f84
Lucas Alves <lucasalves_taz@hotmail.com>
2021-11-01 18:38:55 -0300

    add

---
+++ SRC/__conectheroku__.py
@@ -0,0 +1,22 @@
+import psycopg2
+import os
+import dotenv
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+
+def conexao():
+    con = psycopg2.connect(
+        user=os.getenv("USER"),
+        password=os.getenv("PASSWORD"),
+        database=os.getenv("DATABASE"),
+        host=os.getenv("HOST"),
+    )
+    return con
+
+def in_dados(query):
+    vcon = conexao()
+    c = vcon.cursor()
+    c.execute(query)
+    vcon.commit()
+    vcon.close()
+   
--- SRC/__log__.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-ch = logging.StreamHandler()
-ch.setFormatter(formatter)
-log.addHandler(ch)
