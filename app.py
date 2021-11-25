b10960bd13e781c3621112d84fbb1f7dd22cae66
Lucas Alves <lucasalves_taz@hotmail.com>
2021-11-25 18:48:58 -0300

    add

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas as pd
-from datetime import date
+from datetime import date, timedelta
 
-today = date.today()
+dt = date.today() - timedelta(days=1)
 df = pd.read_csv('./Backup/some_file.csv', sep=';', encoding='ISO-8859-1')
 
 
-view = df.query(f' data_dado_inserido = {today} ')
+view = df.query(f' data_dado_inserido = {dt} ')
 
 st.dataframe(view)
