07cfac64cb66706c95c5a21ee5e547d6ed303bd7
Lucas Alves <lucasalves_taz@hotmail.com>
2021-11-25 18:38:52 -0300

    add

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
+from datetime import date
 
+today = date.today()
 df = pd.read_csv('./Backup/some_file.csv', sep=';', encoding='ISO-8859-1')
 
-st.dataframe(df)
+
+view = df.query(f' data_dado_inserido = {today} ')
+
+st.dataframe(view)
