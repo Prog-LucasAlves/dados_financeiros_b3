bef8a743d9b9c0a2fd17b8ad14ae1b66964b5b27
Lucas Alves <lucasalves_taz@hotmail.com>
2021-11-25 20:10:02 -0300

    add

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import date, timedelta
 
-dt = date.today() - timedelta(days=1)
+dt = date.today() - timedelta(days=0)
 df = pd.read_csv('./Backup/some_file.csv', sep=';', encoding='ISO-8859-1')
 
 
