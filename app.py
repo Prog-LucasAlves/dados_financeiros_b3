4da81ecf785bf398d84fdb2f91c191a41c85788a
Lucas Alves <lucasalves_taz@hotmail.com>
2021-11-26 09:44:45 -0300

    add

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import date, timedelta
 dt = date.today() - timedelta(days=0)
 df = pd.read_csv('./Backup/some_file.csv', sep=';', encoding='ISO-8859-1')
 
-
+st.markdown('Dados Financeiros das Ações listadas na bolsa brasileira')
 view = df[(df['data_dado_inserido'] == '2021-11-25')]
 
 st.dataframe(view)
