da67e4748f3e59123b1d04d23856fd817a82816e
Lucas Alves <lucasalves_taz@hotmail.com>
2021-11-27 13:13:02 -0300

    add

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 #####
-st.markdown('Dados Financeiros das Ações listadas na bolsa brasileira')
+st.subheader('Dados Financeiros das Ações listadas na bolsa brasileira')
 
 #####
 df = pd.read_csv('./Dados_Atual/dados.csv', sep=';')
@@ -10,3 +10,9 @@ df = pd.read_csv('./Dados_Atual/dados.csv', sep=';')
 # Cria barra lateral
 st.sidebar.header("Escolha sua ação")
 col1_selection = st.sidebar.selectbox("Papel", df.papel, list(df.papel).index("AALR3"))
+
+# Cria colunas
+col1 , col2 = st.columns(2)
+
+
+    
