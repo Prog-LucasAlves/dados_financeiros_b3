42ed34cb032d8835fff78a359a82f3d352878967
Lucas Alves <lucasalves_taz@hotmail.com>
2021-11-26 22:51:35 -0300

    add

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import streamlit as st
 import pandas as pd
 
 #####
-st.markdown('Dados Financeiros das Ações listadas na bolsa brasileira')
+st.title('Dados Financeiros das Ações listadas na bolsa brasileira')
 
 #####
-df = pd.read_csv('./Dados_Atual/dados.csv', sep=';',)
-st.dataframe(df)
+df = pd.read_csv('./Dados_Atual/dados.csv', sep=';')
 
 # Cria barra lateral
 st.sidebar.header("Escolha sua ação")
