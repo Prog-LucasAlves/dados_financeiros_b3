9f8453ad3813287f8d7d514083abf3200594bdf1
Lucas Alves <lucasalves_taz@hotmail.com>
2021-11-27 19:59:52 -0300

    add

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ papel_index = int(papel['Unnamed: 0'])
 papel_result = papel['papel'][papel_index]
 col1.metric(label="Papel", value = papel_result)
 
-#col2.1 - 
+#col2.1 - empresa
 empresa = df[df['empresa'] == col1_selection]
 empresa_index = int(empresa['Unnamed: 0'])
 empresa_result = empresa['empresa'][empresa_index]
