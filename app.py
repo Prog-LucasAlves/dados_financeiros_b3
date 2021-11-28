08f38ad448a57996d0032e1e0d4c70396ee63105
Lucas Alves <lucasalves_taz@hotmail.com>
2021-11-27 22:31:42 -0300

    add

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ col1_selection = st.sidebar.selectbox("Papel", df.papel, list(df.papel).index("A
 # Cria colunas
 col1 , col2 = st.columns(2)
 
-#col1.1 - papel
-papel = df[df['papel'] == col1_selection]
-papel_index = int(papel['Unnamed: 0'])
-papel_result = papel['papel'][papel_index]
-col1.metric(label="Papel", value = papel_result)
+#col1.1 - tipo
+tipo = df[df['papel'] == col1_selection]
+tipo_index = int(tipo['Unnamed: 0'])
+tipo_result = tipo['tipo'][tipo_index]
+col1.metric(label="Tipo", value = tipo_result)
 
 #col2.1 - empresa
 empresa = df[df['papel'] == col1_selection]
@@ -27,14 +27,3 @@ empresa_index = int(empresa['Unnamed: 0'])
 empresa_result = empresa['empresa'][empresa_index]
 col2.metric(label="Empresa", value = empresa_result)
 
-#col1.2 - tipo
-tipo = df[df['papel'] == col1_selection]
-tipo_index = int(tipo['Unnamed: 0'])
-tipo_result = tipo['tipo'][tipo_index]
-col1.metric(label="Tipo", value = tipo_result)
-
-#col2.2 - setor
-setor = df[df['papel'] == col1_selection]
-setor_index = int(setor['Unnamed: 0'])
-setor_result = setor['setor'][setor_index]
-col2.metric(label="Setor", value = setor_result)
