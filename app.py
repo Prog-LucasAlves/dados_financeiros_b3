1b46753a3393dff79e91e499f6b735e0c9854f8e
Lucas Alves <lucasalves_taz@hotmail.com>
2021-11-29 12:18:20 -0300

    add

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ df = pd.read_csv('./Dados_Atual/dados.csv', sep=';')
 
 # Cria barra lateral
 st.sidebar.header("Escolha sua ação")
-col1_selection = st.sidebar.selectbox("Papel", df.papel, list(df.papel).index("AALR3"))
+col1_selection = st.sidebar.selectbox("Papel", df.papel, list(df.papel).index("ADHM3"))
 
 # Cria colunas
 col1 , col2 = st.columns(2)
