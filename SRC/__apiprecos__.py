2c98bebe6bb7ea1a69be1ba001370244ba4c6984
Lucas Alves <lucasalves_taz@hotmail.com>
2021-11-30 20:05:44 -0300

    add

---
+++ SRC/__apiprecos__.py
@@ -0,0 +1,21 @@
+import yfinance as yf
+import pandas as pd
+from datetime import date
+from tqdm import tqdm
+
+import __list__
+
+df = pd.DataFrame()
+
+inicio = '2000-01-01'
+fim = date.today()
+
+acao = __list__.lst_acao
+
+for i in tqdm(acao):
+    df = yf.download(f'{i}.SA', start=inicio, end=fim)
+    df.to_csv(f'../SRC/precos/{i}.csv',sep=';')
+
+ticker = 'BVSP'
+df_b = yf.download(f'^{ticker}', start=inicio, end=fim)
+df_b.to_csv(f'../SRC/precos/{ticker}.csv',sep=';')  
