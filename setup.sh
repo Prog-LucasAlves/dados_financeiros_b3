ba15c9665815a24f17ec3baef19826b8ba6f8b0a
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2022-05-26 12:10:33 -0300

    ⚡️ v407 - Heroku

--- setup.sh
+++ setup.sh
@@ -5,23 +5,16 @@ echo "\
 email = \"seu-email@dominio.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-
-[theme]\n\
-# Primary accent for interactive elements
-primaryColor = '#7792E3'
-# Background color for the main content area
-backgroundColor = '#273346'
-# Background color for sidebar and most interactive widgets
-secondaryBackgroundColor = '#B9F1C0'
-# Color used for almost all text
-textColor = '#FFFFFF'
-# Font family for all text in the app, except code blocks
-# Accepted values (serif | sans serif | monospace) 
-# Default: 'sans serif'
+echo 
+"[theme]
+primaryColor = ' #84a3a7 '
+backgroundColor = ' #7b9971 '
+secondaryBackgroundColor = ' #fafafa '
+textColor= ' #424242 '
 font = 'sans serif'
+
+[servidor]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
