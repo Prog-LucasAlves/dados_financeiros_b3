3795851b129d46e79cad54f15878f6efb5a9ef80
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2022-05-26 12:19:48 -0300

    ⚡️ v408 - Heroku

--- setup.sh
+++ setup.sh
@@ -7,10 +7,10 @@ email = \"seu-email@dominio.com\"\n\
 
 echo 
 "[theme]
-primaryColor = ' #84a3a7 '
-backgroundColor = ' #7b9971 '
-secondaryBackgroundColor = ' #fafafa '
-textColor= ' #424242 '
+primaryColor = '#84a3a7'
+backgroundColor = '#7b9971'
+secondaryBackgroundColor = '#fafafa'
+textColor= '#424242'
 font = 'sans serif'
 
 [servidor]
