b3b99f115857405b79d5e4de18f529ec86fbd2ce
Prog-LucasAlves <lucasalves_taz@hotmail.com>
2022-05-26 11:43:43 -0300

    ⚡️ v405 - Heroku

--- setup.sh
+++ setup.sh
@@ -15,15 +15,15 @@ port = $PORT\n\
 echo "\
 [theme]\n\
 # Primary accent for interactive elements
-primaryColor = '#7792E3'\n\
+primaryColor = '#7792E3'
 # Background color for the main content area
-backgroundColor = '#273346'\n\
+backgroundColor = '#273346'
 # Background color for sidebar and most interactive widgets
-secondaryBackgroundColor = '#B9F1C0'\n\
+secondaryBackgroundColor = '#B9F1C0'
 # Color used for almost all text
-textColor = '#FFFFFF'\n\
+textColor = '#FFFFFF'
 # Font family for all text in the app, except code blocks
 # Accepted values (serif | sans serif | monospace) 
-# Default: 'sans serif'\n\
-font = 'sans serif'\n\
+# Default: 'sans serif'
+font = 'sans serif'
 " > ~/.streamlit/config.toml
